158f0a0908caabd4b7fa983a95d7e060934f9dd1
squillwah <Swagapotato@gmail.com>
2025-09-11 15:41:07 -0400

    reduce jitter by writing display to string before printing

--- main.cpp
+++ main.cpp
@@ -1,34 +1,72 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <cmath>
 
 struct Screen {
     static const int WIDTH = 48*2;
     static const int HEIGHT = 27*2;
-    static const int REFRESH = 30;
+    static const int REFRESH = 10;
     char buffer[HEIGHT][WIDTH];
-} screen;
+};
 
+int run(Screen& screen);
 void clearBuffer(Screen& scr);
 void renderFrame(const Screen& scr);
 void drawCircle(Screen& scr, int x, int y, int r);
 
 int main() {
-    clearBuffer(screen);
+    Screen scr;
+    clearBuffer(scr);
+    
+    switch (run(scr)) {
+        case 0:
+            std::cout << "quit" << std::endl;
+            break;
+        default:
+            break;
+    }
   
+    return 0;
+}
+
+int run(Screen& screen) {
     while (true) { 
         for (int i = 0; i < screen.WIDTH/2; i++) {
             std::this_thread::sleep_for(std::chrono::milliseconds(1000/screen.REFRESH));
-            drawCircle(screen, 10+i, 15, 4);
+            drawCircle(screen, 10+i, 15+std::sin(i)*10, 4);
             renderFrame(screen);
             clearBuffer(screen);
         }
     }
-    
+
     return 0;
 }
 
 void drawCircle(Screen& scr, int x, int y, int r) {
+
+/*          
+ *        XXXXXXX
+ *      XXXXXXXXXXX
+ *     XXXXXXXXXXXXX
+ *     XXXXXXXXXXXXX
+ *     XXXXXXXXXXXXX
+ *      XXXXXXXXXXX
+ *        XXXXXXX
+ *
+ *         X X X X
+ *       X X X X X X
+ *       X X X X X X
+ *       X X X X X X
+ *         X X X X X
+ *         
+ */
+
+
+//    for (int i = 0; i < r; i++) {
+//        for (int i = 0; i < 
+//
+
     for (int i = 0; i < r; i++) {
         scr.buffer[y+i][x] = 'X';
         scr.buffer[y-i][x] = 'X';
@@ -46,23 +84,34 @@ void clearBuffer(Screen& scr) {
 }
 
 void renderFrame(const Screen& scr) {
-    std::cout << '/';
-    for (int i = 0; i < scr.WIDTH*2+1; i++)
-        std::cout << '-'; 
-    std::cout << '\\';
-    std::cout << '\n';
+    char frameString[(scr.WIDTH*2+4)*(scr.HEIGHT+2)];
+    int p = 0;
+    
+    frameString[p] = '/'; p++;
+    for (int i = 0; i < scr.WIDTH*2+1; i++) {
+        frameString[p] = '-'; p++;
+    }
+    frameString[p] = '\\'; p++;
+    frameString[p] = '\n'; p++;
 
     for (int i = 0; i < scr.HEIGHT; i++) {
-        std::cout << '|' << ' ';
-        for (int j = 0; j < scr.WIDTH; j++) 
-            std::cout << scr.buffer[i][j] << ' ';
-        std::cout << '|' << '\n';
+        frameString[p] = '|'; p++;
+        frameString[p] = ' '; p++;
+        for (int j = 0; j < scr.WIDTH; j++) {
+            frameString[p] = scr.buffer[i][j]; p++; 
+            frameString[p] = ' '; p++;
+        }
+        frameString[p] = '|'; p++;
+        frameString[p] = '\n'; p++;
     }
    
     
-    std::cout << '\\';
-    for (int i = 0; i < scr.WIDTH*2+1; i++)
-        std::cout << '-'; 
-    std::cout << '/';
-    std::cout << std::endl;
+    frameString[p] = '\\'; p++;
+    for (int i = 0; i < scr.WIDTH*2+1; i++) {
+        frameString[p] = '-'; p++;
+    }
+    frameString[p] = '/'; p++;
+    frameString[p] = '\0';
+    std::cout << frameString << std::endl;
 }
+
